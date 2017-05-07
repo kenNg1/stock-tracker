9dd646baf7f8c9d199486aa7f7c465e640ba7d30
kenNg1 <kng1230@hotmail.com>
2017-05-07 13:35:33 +0800

    added friends model, search form and js

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -9,4 +11,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends' to: "users#search"
+  post 'add_friend', to: "users#add_friend"
+  
 end
